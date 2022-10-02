85e2be43a138a38a4da1462d40bae799fd452971
uvsq21916945 <anne.duveau@ens.uvsq.fr>
2022-10-02 22:03:36 +0200

    ajout de fichiers.java

---
+++ Fraction.java
@@ -0,0 +1,15 @@
+public class Fraction {
+
+   /*  private int numerateur;
+    private int denominateur;
+
+
+    Fraction(int numerateur, int denominateur){
+
+        numerateur = this.numerateur;
+        denominateur = this.denominateur;
+
+    } */
+
+
+}
